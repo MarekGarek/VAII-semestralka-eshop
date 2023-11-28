dd4a2feaf15abf0be5e039c2b8d73ee980a1ffc1
Marek Garek <garek.marek0077@gmail.com>
2023-11-28 16:18:58 +0100

    Test commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         <>
         <Carousel/>
         <div class="grid-container1">
-            <div class="grid-item1"><i class="bi bi-bag-heart-fill"> 20M+ <em class="odsek-text">spokojných zákazníkov &nbsp;&nbsp;</em></i></div>
+            <div class="grid-item1"><i class="bi bi-bag-heart-fill"> 25M+ <em class="odsek-text">spokojných zákazníkov &nbsp;&nbsp;</em></i></div>
             <div class="grid-item1"><i class="bi bi-hourglass-split"> 24 hodín <em class="odsek-text">rýchle dodanie &nbsp;&nbsp;</em></i></div>
             <div class="grid-item1"><i class="bi bi-truck">doprava zadarmo <em class="odsek-text">pri nákupe nad 20e &nbsp;&nbsp;</em></i></div>
             <div class="grid-item1"><i class="bi bi-bag-fill">10 000+ <em class="odsek-text">produktov </em></i></div>
